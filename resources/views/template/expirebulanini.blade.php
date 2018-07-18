f06a1933c7d36eb26abde28b7341022fa77f9206
Viska Tunru <viska.tunru@gmail.com>
2018-07-18 22:29:58 +0800

    ubah styling expired di beranda

--- resources/views/template/expirebulanini.blade.php
+++ resources/views/template/expirebulanini.blade.php
@@ -4,7 +4,7 @@
 		@foreach($expires as $e)
 			<div class="activity-row">
 				<div class="col-xs-12 activity-desc">
-				<h5><u><a href="/barang/show/{{$e->barang->id}}">{{$e->barang->kode}} - {{$e->barang->nama}}</a></u></h5>
+				<h5 id="barangExpire"><a href="/barang/show/{{$e->barang->id}}">{{$e->barang->kode}} - {{$e->barang->nama}}</a></h5>
 					<p>Tanggal Expired: {{date('j F Y', strtotime($e->tanggal))}}</p>
 					<p>Stok Expired: {{$e->jumlah}}</p>
 					<p>Penyimpanan: {{$e->penyimpanan->nama}}</p>
